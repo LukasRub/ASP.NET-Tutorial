3cbe20d421dea5fc3d4b3eb2a0e4af9265e2aed3
Lukas Rubikas <lukas.rubikas@gmail.com>
2015-10-25 15:15:31 +0200

    Initial

---
+++ ASP.NET Tutorial/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP.NET_Tutorial.Startup))]
+namespace ASP.NET_Tutorial
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
